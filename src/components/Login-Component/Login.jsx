39dd253a6bc6e959f8f8df602364f8774b3290a1
Junior Vinicius <jr.vinicius.silva@gmail.com>
2021-12-30 21:33:14 -0300

    Inicio da tela de login

---
+++ src/components/Login-Component/Login.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./style.css";
+import logo from "../../img/logo.png";
+import {AiOutlineUser, AiOutlineLock } from "react-icons/ai";
+
+
+export default function Login (){
+    return (
+        <div>
+            <div id="conteiner">
+                <figure>
+                    <img src={logo} alt="Forest Logo" />
+                </figure>
+
+                <form>
+                    <div className="box-input">
+                        <AiOutlineUser size={20} color="#224C3B"/>
+                        <input type="text" placeholder="Username"/>
+                    </div>
+                    <div className="box-input">
+                        <AiOutlineLock size={20} color="#224C3B"/>
+                        <input type="password" placeholder="Username"/>
+                    </div>
+                    <div>
+                        <input id="button" type="submit" value="SIGN IN"/>
+
+                        <p>Don’t have an account? <span>Sing up</span></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
