065cf8bd3006d3b2ebb66b4a199e64836e53ef85
Junior Vinicius <jr.vinicius.silva@gmail.com>
2022-01-03 15:25:05 -0300

    tela profile e botão sair

--- src/components/pages/profile-user/Profile.jsx
+++ src/components/pages/profile-user/Profile.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
+import "./ProfileStyle.css";
+import logo from "../../../img/white-logo.png";
+
+import StoreContext from "../../context/Context";
+
 
 export default function Profile() {
+  const { setToken } = useContext(StoreContext);
+
   return (
-    <div>
-      <h1>Cool!!!</h1>
+    <div id="background">
+      <div id="content">
+      <figure>
+          <img src={logo} alt="Forest Logo" title="Protect the forest!" />
+        </figure>
+
+        <div id="userInitial">
+          <span>A</span>
+        </div>
+
+        <div id="text-conteiner">
+          <h1>WELCOME</h1>
+          <span>ADMIN</span>
+        </div>
+
+        <button onClick={() => setToken(null)}>QUIT</button>
+
+      </div>
     </div>
   );
 }
